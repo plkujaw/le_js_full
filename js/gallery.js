fe4c1fc55b9348fabb8752e18c3560fc59b4c60b
plkujaw <jawor.jakub@gmail.com>
2020-06-19 13:13:32 +0100

    add main image changing on thumbnail click

--- js/gallery.js
+++ js/gallery.js
@@ -1,10 +1,11 @@
 function activateGallery() {
   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
-
+  let mainImage = document.querySelector("#gallery-photo img");
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function() {
       // set clicked image as main image
-      
-    });
+      let newImageSrc = thumbnail.dataset.largeVersion;
+    mainImage.setAttribute("src", newImageSrc);
+  });
   });
 }
