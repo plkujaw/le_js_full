dc62b4bc7165cdde2402a1a82a5f5d23818c7309
plkujaw <jawor.jakub@gmail.com>
2020-06-19 14:11:30 +0100

    sync new image title and description

--- js/gallery.js
+++ js/gallery.js
@@ -5,16 +5,25 @@ function activateGallery() {
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function() {
 
-      // set clicked image as main image
       let newImageSrc = thumbnail.dataset.largeVersion;
-      let newImageAlt = thumbnail.alt;
       let currentClass = "current";
+      let newImageAlt = thumbnail.alt;
+
+      // set clicked image as main image
       mainImage.setAttribute("src", newImageSrc);
       mainImage.setAttribute("alt", newImageAlt);
 
       // change which image is current (for thumbnail border)
       document.querySelector(".current").classList.remove(currentClass);
       thumbnail.parentNode.classList.add(currentClass);
+
+      // update information for each image on click
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
+      title.innerHTML       = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
     });
   });
 }
