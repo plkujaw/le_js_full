ad5a42df297bb5afd20880ff395fdda202041260
plkujaw <jawor.jakub@gmail.com>
2020-06-19 13:39:24 +0100

    add thumbnail frame update to match current image

--- js/gallery.js
+++ js/gallery.js
@@ -1,14 +1,20 @@
 function activateGallery() {
   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
   let mainImage  = document.querySelector("#gallery-photo img");
+
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function() {
+
       // set clicked image as main image
       let newImageSrc = thumbnail.dataset.largeVersion;
       let newImageAlt = thumbnail.alt;
+      let currentClass = "current";
       mainImage.setAttribute("src", newImageSrc);
       mainImage.setAttribute("alt", newImageAlt);
-      console.log(newImageAlt);
-  });
+
+      // change which image is current (for thumbnail border)
+      document.querySelector(".current").classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+    });
   });
 }
