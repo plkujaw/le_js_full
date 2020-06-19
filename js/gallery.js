3ae03364c85934302d8297529d0bc095b79ec1af
plkujaw <jawor.jakub@gmail.com>
2020-06-19 12:41:06 +0100

    add eventListener for click on thumbnail

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,10 @@
 function activateGallery() {
-  alert('hello gallery');
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      // set clicked image as main image
+      
+    });
+  });
 }
