78f03c7d5fb425e88d5106e76d3eaa9dda497896
plkujaw <jawor.jakub@gmail.com>
2020-06-19 13:26:27 +0100

    add alt aatribute update for new image

--- js/gallery.js
+++ js/gallery.js
@@ -1,11 +1,14 @@
 function activateGallery() {
   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
-  let mainImage = document.querySelector("#gallery-photo img");
+  let mainImage  = document.querySelector("#gallery-photo img");
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function() {
       // set clicked image as main image
       let newImageSrc = thumbnail.dataset.largeVersion;
-    mainImage.setAttribute("src", newImageSrc);
+      let newImageAlt = thumbnail.alt;
+      mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", newImageAlt);
+      console.log(newImageAlt);
   });
   });
 }
